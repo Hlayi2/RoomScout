9cef464f23725c2879e294fd15afb707cbf46465
Hlayi2 <katlegokatenguyaza01@gmail.com>
2024-11-18 15:54:02 +0200

    here is the register page , it connects with the landlordDashborad. you can now pull.

--- RoomScout/AppShell.xaml.cs
+++ RoomScout/AppShell.xaml.cs
@@ -1,10 +1,18 @@
-﻿namespace RoomScout
+﻿using RoomScout.Views.AdminSide;
+using RoomScout.Views.Auth;
+using RoomScout.Views.StudentSide;
+
+namespace RoomScout
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute("register", typeof(RoomScout.Views.Auth.RegisterPage));
+            Routing.RegisterRoute("landlordDashboard", typeof(RoomScout.Views.AdminSide.LandlordDashboardPage));
+            Routing.RegisterRoute("viewApartments", typeof(RoomScout.Views.StudentSide.BrowseListingsPage));
         }
     }
 }
