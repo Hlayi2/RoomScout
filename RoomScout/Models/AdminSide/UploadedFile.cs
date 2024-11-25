4282c6f75f0eb6374f8a7c070c7887c1608ac8cd
BerniceMlangeni <bernicemlangeni@gmail.com>
2024-11-25 09:15:53 +0200

    Added more stuff on landlord listings

---
+++ RoomScout/Models/AdminSide/UploadedFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomScout.Models.AdminSide
+{
+    public class UploadedFile
+    {
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public string FileType { get; set; }
+        public long FileSize { get; set; }
+        public bool IsCertified { get; set; }
+        public DateTime UploadDate { get; set; }
+    }
+}
---
+++ RoomScout/Services/Common/IFileUploadService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomScout.Services.Common
+{
+    public interface IFileUploadService
+    {
+        Task<Models.AdminSide.UploadedFile> UploadFileAsync(FileResult file, string purpose);
+        Task<bool> ValidateFileAsync(FileResult file);
+    }
+}
