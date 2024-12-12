b9fb9caef351c924fa56da4bc2e82e127a92624e
Hlayi2 <katlegokatenguyaza01@gmail.com>
2024-12-12 18:43:48 +0200

    new developments

--- RoomScout1-master/RoomScout1-master/RoomScout/Views/Auth/LandingPage.xaml.cs
+++ RoomScout1-master/RoomScout1-master/RoomScout/Views/Auth/LandingPage.xaml.cs
@@ -2,12 +2,37 @@ namespace RoomScout.Views.Auth;
 
 public partial class LandingPage : ContentPage
 {
+    private const string IsLandingPageViewedKey = "IsLandingPageViewed";
+
     public LandingPage()
     {
         InitializeComponent();
+        VersionTracking.Track();
+
+        if (!VersionTracking.IsFirstLaunchEver)
+        {
+            NavigateToLoginPageAsync();
+        }
+         
     }
+
+
+
+    private async void NavigateToLoginPageAsync()
+    {
+      
+       
+        {
+            // Navigate to the LoginPage
+            await AppShell.Current.GoToAsync("//login");
+        }
+    }
+
     private async void OnGetStartedClicked(object sender, EventArgs e)
     {
+        
+
+         // Navigate to the registration page
         await AppShell.Current.GoToAsync("register");
     }
 }
